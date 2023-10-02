176c7a1908561b0f46716a7a3e6e5cb13d3666ee
andersbohnm <anders.jespersen@metaco.com>
2023-10-02 11:11:48 +0200

    four combos - at least one is wrong

--- src/main/scala/com/softwaremill/Endpoints.scala
+++ src/main/scala/com/softwaremill/Endpoints.scala
@@ -35,13 +35,59 @@ object Endpoints {
 }
 
 object Library {
-  case class Author(name: String)
-  case class Book(title: String, @Schema.annotations.deprecated year: Int, @Schema.annotations.deprecated author: Author)
+  case class AZ(name: String)
+  case class BZ(name: String)
+  case class CZ(name: String)
+  case class DZ(name: String)
+  case class Book(
+      title: String,
+      @Schema.annotations.deprecated year: Int,
+      a1: AZ,
+      a2: Option[AZ],
+      @Schema.annotations.deprecated b1: BZ,
+      b2: Option[BZ],
+      c1: CZ,
+      @Schema.annotations.deprecated c2: Option[CZ],
+      @Schema.annotations.deprecated d1: DZ,
+      @Schema.annotations.deprecated d2: Option[DZ]
+  )
 
   val books = List(
-    Book("The Sorrows of Young Werther", 1774, Author("Johann Wolfgang von Goethe")),
-    Book("On the Niemen", 1888, Author("Eliza Orzeszkowa")),
-    Book("The Art of Computer Programming", 1968, Author("Donald Knuth")),
-    Book("Pharaoh", 1897, Author("Boleslaw Prus"))
+    Book(
+      "A The Sorrows of Young Werther",
+      1774,
+      AZ("A a1"),
+      Some(AZ("A a2")),
+      BZ("A b1"),
+      Some(BZ("A b2")),
+      CZ("A c1"),
+      Some(CZ("A c2")),
+      DZ("A d1"),
+      Some(DZ("A d2"))
+    ),
+    Book(
+      "B On the Niemen",
+      1888,
+      AZ("B a1"),
+      Some(AZ("B a2")),
+      BZ("B b1"),
+      Some(BZ("B b2")),
+      CZ("B c1"),
+      Some(CZ("B c2")),
+      DZ("B d1"),
+      Some(DZ("B d2"))
+    ),
+    Book(
+      "C The Art of Computer Programming",
+      1968,
+      AZ("C a1"),
+      Some(AZ("C a2")),
+      BZ("C b1"),
+      Some(BZ("C b2")),
+      CZ("C c1"),
+      Some(CZ("C c2")),
+      DZ("C d1"),
+      Some(DZ("C d2"))
+    )
   )
 }
