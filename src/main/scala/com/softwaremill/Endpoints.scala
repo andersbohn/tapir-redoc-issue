afe7a23158ed1b013c8561ca476c01ac283a84b7
andersbohnm <anders.jespersen@metaco.com>
2023-10-06 09:20:33 +0200

    simplest illustration

--- src/main/scala/com/softwaremill/Endpoints.scala
+++ src/main/scala/com/softwaremill/Endpoints.scala
@@ -1,38 +1,28 @@
 package com.softwaremill
 
-import sttp.tapir._
-import Library._
+import com.softwaremill.Library._
 import io.circe.generic.auto._
-import sttp.tapir.Schema.annotations.encodedName
-import sttp.tapir.generic.auto._
+import sttp.tapir._
 import sttp.tapir.json.circe._
-import sttp.tapir.redoc.bundle.RedocInterpreter
-import sttp.tapir.redoc.RedocUIOptions
-import sttp.tapir.redoc.bundle.RedocInterpreter
 import sttp.tapir.swagger.bundle.SwaggerInterpreter
 import sttp.tapir.ztapir.ZServerEndpoint
-import zio.Task
-import zio.ZIO
+import zio.{Task, ZIO}
 
 object Endpoints {
-  case class User(name: String) extends AnyVal
-  val helloEndpoint: PublicEndpoint[User, Unit, String, Any] = endpoint.get
-    .in("hello")
-    .in(query[User]("name"))
-    .out(stringBody)
-  val helloServerEndpoint: ZServerEndpoint[Any, Any] = helloEndpoint.serverLogicSuccess(user => ZIO.succeed(s"Hello ${user.name}"))
 
-  val booksListing: PublicEndpoint[Unit, Unit, List[Book], Any] = endpoint.get
-    .in("books" / "list" / "all")
-    .out(jsonBody[List[Book]])
-  val booksListingServerEndpoint: ZServerEndpoint[Any, Any] = booksListing.serverLogicSuccess(_ => ZIO.succeed(List.empty[Book]))
-
-  val apiEndpoints: List[ZServerEndpoint[Any, Any]] = List(helloServerEndpoint, booksListingServerEndpoint)
+  val bookAsListing: PublicEndpoint[Unit, Unit, List[BookA], Any] = endpoint.get
+    .in("bookAs" / "list" / "all")
+    .out(jsonBody[List[BookA]])
+  val bookBsListing: PublicEndpoint[Unit, Unit, List[BookB], Any] = endpoint.get
+    .in("bookBs" / "list" / "all")
+    .out(jsonBody[List[BookB]])
+  val apiEndpoints: List[ZServerEndpoint[Any, Any]] = List(
+    bookAsListing.serverLogicSuccess(_ => ZIO.succeed(List.empty[BookA])),
+    bookBsListing.serverLogicSuccess(_ => ZIO.succeed(List.empty[BookB]))
+  )
 
   val docEndpoints: List[ZServerEndpoint[Any, Any]] = SwaggerInterpreter()
     .fromServerEndpoints[Task](apiEndpoints, "vague-chipmunk", "1.0.0")
-
-//  val docEndpoints: List[ZServerEndpoint[Any, Any]] = RedocInterpreter().fromServerEndpoints[Task](apiEndpoints, "vague-chipmunk", "1.0.0")
 
   val all: List[ZServerEndpoint[Any, Any]] = apiEndpoints ++ docEndpoints
 }
@@ -41,16 +31,19 @@ object Library {
 
   sealed trait CcName
   case class AName(s: String) extends CcName
-  case class BName(i: Int) extends CcName
-  case class Book(
-      title: String,
-      year: Int,
+  case class BName(s: String) extends CcName
+  case class BookA(
       aName: AName,
-      @Schema.annotations.deprecated
-      deprAName: AName,
-      bName: BName,
-      cName: CcName,
-      dName: CcName
+      @Schema.annotations.deprecated deprAName: AName
   )
+  case class BookB(
+      @Schema.annotations.deprecated deprBName: BName,
+      bName: BName
+  )
+
+  implicit val aNameSchema: Schema[AName] = Schema.derived[AName]
+  implicit val bNameSchema: Schema[BName] = Schema.derived[BName]
+  implicit val bookASchema: Schema[BookA] = Schema.derived[BookA]
+  implicit val bookBSchema: Schema[BookB] = Schema.derived[BookB]
 
 }
--- src/test/scala/com/softwaremill/EndpointsSpec.scala
+++ src/test/scala/com/softwaremill/EndpointsSpec.scala
@@ -2,7 +2,7 @@ package com.softwaremill
 
 import zio.ZIO
 import zio.test.Assertion._
-import zio.test.{ ZIOSpecDefault, assertZIO }
+import zio.test.{ZIOSpecDefault, assertZIO}
 
 import java.nio.charset.StandardCharsets
 import java.nio.file.Files
@@ -14,8 +14,8 @@ object EndpointsSpec extends ZIOSpecDefault {
       import sttp.apispec.openapi.circe.yaml._
       import sttp.tapir.docs.openapi._
       val docs: OpenAPI =
-        OpenAPIDocsInterpreter().toOpenAPI(List(Endpoints.helloEndpoint, Endpoints.booksListing), "vague-chipmunk", "1.0.0")
-      if (true) {
+        OpenAPIDocsInterpreter().toOpenAPI(List(Endpoints.bookAsListing, Endpoints.bookBsListing), "vague-chipmunk", "1.0.0")
+      if (false) {
         Files.write(java.nio.file.Paths.get("gen-docs.yaml"), docs.toYaml.getBytes(StandardCharsets.UTF_8))
       }
       val expected = scala.io.Source.fromResource("docs.yaml").mkString
