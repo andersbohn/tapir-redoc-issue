29e8f832ff17f3ccb2077f201bdf568222d70db0
andersbohnm <anders.jespersen@metaco.com>
2023-10-24 08:47:25 +0200

    weird npe?!

--- build.sbt
+++ build.sbt
@@ -1,4 +1,4 @@
-val tapirVersion = "1.7.5"
+val tapirVersion = "1.8.2"
 
 lazy val rootProject = (project in file(".")).settings(
   Seq(
--- src/main/scala/com/softwaremill/Endpoints.scala
+++ src/main/scala/com/softwaremill/Endpoints.scala
@@ -14,7 +14,9 @@ object Endpoints {
     .in("events" / "list" / "all")
     .out(jsonBody[List[Event]])
   val postEvent = endpoint.post
-    .in("events").in(jsonBody[Event]).out(emptyOutput)
+    .in("events")
+    .in(jsonBody[Event])
+    .out(emptyOutput)
 
   val apiEndpoints: List[ZServerEndpoint[Any, Any]] = List(
     eventsListing.serverLogicSuccess(_ => ZIO.succeed(List.empty[Event])),
@@ -38,8 +40,9 @@ object Library {
   case object EventTypeB extends EventType
 
   sealed trait EventParameters
-  case class EventParametersA(s:String) extends EventParameters
-  case class EventParametersB(@Schema.annotations.deprecated deprBName: BName) extends EventParameters
+  case class EventParametersA(s: String) extends EventParameters
+//  case class EventParametersB(@Schema.annotations.deprecated deprBName: BName) extends EventParameters
+  case class EventParametersB(deprBName: BName) extends EventParameters
 
   case class CreateEvent(eventType: EventType, eventToCreate: Event)
 
@@ -57,13 +60,13 @@ object Library {
       deprAName: AName,
       bName: BName,
       eventParameters: EventParametersB
-  )extends Event
+  ) extends Event
   case class EventC(
 //      @Schema.annotations.deprecated
       deprBName: BName,
       bName: BName,
       eventParameters: EventParametersA
-  )extends Event
+  ) extends Event
   case class EventD(
       aName: BName,
       bName: BName,
@@ -72,14 +75,14 @@ object Library {
 
   implicit val eventTypeParametersASchema: Schema[EventParametersA] = Schema.derived[EventParametersA]
   implicit val eventTypeParametersBSchema: Schema[EventParametersB] = Schema.derived[EventParametersB]
-//  implicit val eventTypeASchema: Schema[EventTypeA] = Schema.derived[EventTypeA]
-//  implicit val eventTypeBSchema: Schema[EventTypeB] = Schema.derived[EventTypeB]
+  implicit val eventTypeSchema: Schema[EventType] = Schema.derived[EventType]
   implicit val aNameSchema: Schema[AName] = Schema.derived[AName]
   implicit val bNameSchema: Schema[BName] = Schema.derived[BName]
   implicit val eventASchema: Schema[EventA] = Schema.derived[EventA]
-  implicit val eventBSchema: Schema[EventB] = Schema.derived[EventB]// TODO wut? .modify(_.deprAName)(_.name(None)).name(None)
+  implicit val eventBSchema: Schema[EventB] = Schema.derived[EventB] // TODO wut? .modify(_.deprAName)(_.name(None)).name(None)
   implicit val eventCSchema: Schema[EventC] = Schema.derived[EventC]
   implicit val eventDSchema: Schema[EventD] = Schema.derived[EventD]
   implicit val eventSchema: Schema[Event] = Schema.derived[Event]
+  implicit val createEventSchema: Schema[CreateEvent] = Schema.derived[CreateEvent]
 
 }
